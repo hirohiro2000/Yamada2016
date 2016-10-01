54adc74d3164d0ee3bd59f723b164896ae7cee45
marumaru-pg <marumaru.pg@gmail.com>
2016-10-01 19:55:17 +0900

    オブジェクト配置に時間を追加

--- RTS_And_TPS/Assets/Mechanical girl/Default/MechanocalGirl.cs
+++ RTS_And_TPS/Assets/Mechanical girl/Default/MechanocalGirl.cs
@@ -1,21 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MechanocalGirl : MonoBehaviour 
 {
+	public	GameObject	m_resActionBar	= null;
+	private GameObject	m_actionBar		= null;
+
+	private const KeyCode		m_putKey		= KeyCode.Z;
+	private const KeyCode		m_createKey		= KeyCode.X;
+
+	private enum ActionState
+	{
+		Common,
+		PutResource,
+		CreateResource,
+	}
+	private ActionState m_actionState = ActionState.Common;
+
+
 	// Use this for initialization
 	void Start ()
 	{
+		m_actionBar						= Instantiate( m_resActionBar );
+		m_actionBar.transform.parent	= GameObject.Find("Canvas").transform;
+		ChangeActionBarState( false );
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		IsMovedByKey ();
+		switch( m_actionState )
+		{
+		case ActionState.Common:			IsMovedByKey(); break;
+		case ActionState.PutResource:		PutResource();	break;
+		case ActionState.CreateResource:	CreateResource();break;
+		}
 	}
 
+	//
 	void IsMovedByKey()
 	{		
+		//	move
 		float v = Input.GetAxis ("Vertical");
 		float h = Input.GetAxis ("Horizontal");
 
@@ -28,7 +54,8 @@ public class MechanocalGirl : MonoBehaviour
 		float moveSpeed 		= 30.0f;
 		transform.position 		+= direction * axis * moveSpeed * Time.deltaTime;
 
-
+		
+		//	rotate
 		Vector3 animDir 		= direction;
 		animDir.y 				= 0;
 
@@ -37,5 +64,69 @@ public class MechanocalGirl : MonoBehaviour
 			Vector3 newDir 		= Vector3.RotateTowards( transform.forward, animDir, 10.0f*Time.deltaTime, 0f );
 			transform.rotation 	= Quaternion.LookRotation( newDir );
 		}
+
+
+		//	change state
+		if( Input.GetKeyDown( m_putKey ) )
+		{
+			m_actionState = ActionState.PutResource;
+			m_actionBar.GetComponent<ValueSlider>().SetColor( Color.green );
+			ChangeActionBarState( true );
+		}
+		if( Input.GetKeyDown( m_createKey ) )
+		{
+			m_actionState = ActionState.CreateResource;
+			m_actionBar.GetComponent<ValueSlider>().SetColor( Color.cyan );
+			ChangeActionBarState( true );
+		}
+	}
+	void PutResource()
+	{
+		ValueSlider slider = m_actionBar.GetComponent<ValueSlider>();
+		slider.m_pos = transform.position;
+		slider.m_cur += 1.0f;//kari
+
+
+		//	change state
+		if( slider.GetValue() >= 1.0f )
+		{
+			m_actionState = ActionState.Common;
+			ChangeActionBarState( false );
+			return;
+		}
+		if( Input.GetKeyUp( m_putKey ) )
+		{
+			m_actionState = ActionState.Common;
+			ChangeActionBarState( false );
+			return;
+		}
+	}
+	void CreateResource()
+	{
+		ValueSlider slider = m_actionBar.GetComponent<ValueSlider>();
+		slider.m_pos = transform.position;
+		slider.m_cur += 1.0f;//kari
+
+		
+		//	change state
+		if( slider.GetValue() >= 1.0f )
+		{
+			m_actionState = ActionState.Common;
+			ChangeActionBarState( false );
+			return;
+		}
+		if( Input.GetKeyUp( m_createKey ) )
+		{
+			m_actionState = ActionState.Common;
+			ChangeActionBarState( false );
+			return;
+		}
+	}
+
+	//
+	void ChangeActionBarState( bool enable )
+	{
+		m_actionBar.GetComponent<ValueSlider>().m_cur	= 0.0f;
+		m_actionBar.active								= enable;
 	}
 }
