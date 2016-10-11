ef9a5e50ae37ab9ffdf9104d6eb50ef562bbc51a
nakano <h2nakano@gmail.com>
2016-10-11 15:43:30 +0900

    画面中央に弾打って 火花出て 敵倒せますが 敵のrigidbody周りを修正必要あり

---
+++ RTS_And_TPS/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+	[SerializeField]
+	float hp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GiveDamage(float damage)
+	{
+		hp -= damage;
+		if(hp <= .0f)
+		{
+			Destroy(this.gameObject);
+		}
+    }
+}
---
+++ RTS_And_TPS/Assets/Scripts/WeakPoint.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeakPoint : MonoBehaviour
+{
+
+	Enemy enemy;
+	[SerializeField]
+	float damageMultiple;
+	// Use this for initialization
+	void Start()
+	{
+		enemy = GetComponent<Enemy>();
+		if(enemy == null)
+		{
+			enemy = GetComponentInParent<Enemy>();
+		}
+    }
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(gameObject.name);
+		DamageSource source = collision.gameObject.GetComponentInParent<DamageSource>();
+		if(source != null)
+		{
+			enemy.GiveDamage(damageMultiple * source.damage);
+
+		}
+    }
+}
