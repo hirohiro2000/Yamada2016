ae10f35b34f5aeeae3cd7e5860883d65fb56627e
oki <yosi39392410@gmail.com>
2016-10-19 23:23:34 +0900

    RTS：ステージに設置されているオブジェクト配置ゲーム中[LvUP]ボタンで動作させる

---
+++ RTS_And_TPS/Assets/Scripts/Facility/MechanicalGirlFacility.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ResourceParam))]
+[RequireComponent(typeof(CollisionParam))]
+public class MechanicalGirlFacility : Facility
+{
+    protected override void Execute()
+    {
+        Debug.Log("MechanicalGirlFacility.Execute()");
+    }
+
+}
