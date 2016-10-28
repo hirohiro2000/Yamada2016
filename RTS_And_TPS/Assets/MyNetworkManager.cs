3cadd3f23dcb84f6b72726aad9ca26fb08fdc635
Sengoku39 <mindofevil39@gmail.com>
2016-10-28 13:44:57 +0900

    NetworkManagerHUD修正

---
+++ RTS_And_TPS/Assets/MyNetworkManager.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+using System.Collections;
+
+public class MyNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	void    Start()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void    Update()
+    {
+	    
+	}
+}
