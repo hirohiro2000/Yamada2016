7388b1271d5f5817be3cd3b8c245e4838fe24f56
oki <yosi39392410@gmail.com>
2016-11-01 16:23:13 +0900

    リソース追加

---
+++ RTS_And_TPS/Assets/Scripts/UI/TPSHpBar.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TPSHpBar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_mainImage = null;
+
+    float   m_hp;
+    float   m_maxHP;
+
+    private static TPSHpBar instance = null;
+    void Awake()
+    {
+        instance = GetComponent<TPSHpBar>();
+    }
+    void OnEnable()
+    {
+        instance = GetComponent<TPSHpBar>();
+    }
+
+    static public void Initialize(float maxHP)
+    {
+        instance.m_maxHP = maxHP;
+        instance.m_hp = maxHP;
+    }
+    static public void SetHP(float hp)
+    {
+        if (hp < 0.0f)
+        {
+            hp = 0.0f;
+        }
+        float delta = hp - instance.m_hp;
+        float rate = hp / instance.m_maxHP;
+
+        RectTransform rt = instance.m_mainImage.GetComponent<RectTransform>();
+        Vector3 scale = rt.localScale;
+        scale.x = rate;
+        rt.localScale = scale;
+
+        Vector3 localPosition = rt.localPosition;
+        localPosition.x += rt.sizeDelta.x * delta / instance.m_maxHP * 0.5f;
+        rt.localPosition = localPosition;
+
+        instance.m_hp = hp;
+
+    }
+
+}
---
+++ RTS_And_TPS/Assets/Scripts/UI/TPSWeaponBar.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TPSWeaponBar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_mainImage = null;
+
+    float   m_curValue;
+    float   m_capacity;
+
+    private static TPSWeaponBar instance = null;
+    void Awake()
+    {
+        instance = GetComponent<TPSWeaponBar>();
+    }
+    void OnEnable()
+    {
+        instance = GetComponent<TPSWeaponBar>();
+    }
+
+    static public void Initialize(float capacity)
+    {
+        instance.m_capacity = capacity;
+        instance.m_curValue = 0.0f;
+    }
+    static public void Consumption(float value)
+    {
+        instance.m_curValue += value;
+
+        float delta = value;
+        float rate = (instance.m_capacity - instance.m_curValue) / instance.m_capacity;
+
+        RectTransform rt = instance.m_mainImage.GetComponent<RectTransform>();
+        Vector3 scale = rt.localScale;
+        scale.x = rate;
+        rt.localScale = scale;
+
+        Vector3 localPosition = rt.localPosition;
+        localPosition.x += rt.sizeDelta.x * delta / instance.m_capacity * 0.5f;
+        rt.localPosition = localPosition;
+
+        // Clear
+        if (instance.m_curValue >= instance.m_capacity)
+        {
+            instance.m_curValue = 0.0f;
+            rt.localScale = Vector3.one;
+            rt.localPosition = Vector3.zero;
+        }
+
+    }
+
+}
