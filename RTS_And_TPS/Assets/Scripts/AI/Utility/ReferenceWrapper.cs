d65548272ae229f1703d68506ab4a49d38823423
marumaru-pg <marumaru.pg@gmail.com>
2016-11-10 14:49:18 +0900

    2016 11 10

--- RTS_And_TPS/Assets/Scripts/AI/Utility/ReferenceWrapper.cs
+++ RTS_And_TPS/Assets/Scripts/AI/Utility/ReferenceWrapper.cs
@@ -14,10 +14,82 @@ public class ReferenceWrapper : MonoBehaviour {
         m_attack_object_root = GameObject.Find("EnemyAttackObjectRoot");
         m_active_enemy_list = GetComponent<EnemyGenerator>().GetCurrentHierachyList();
     }
-
-   void Update()
+	void Update()
     {
         UserLog.Terauchi(m_active_enemy_list.Count);
     }
 
+
+	public bool IsExistEnemy()
+	{
+		return m_active_enemy_list.Count > 0;
+	}
+	public bool CheckWhetherWithinTheRange( Vector3 target, float rangeDist )
+	{
+		float	near	= rangeDist*rangeDist;
+
+		for( int i=0; i<m_active_enemy_list.Count; ++i )
+		{
+			Vector3 pos		    =	m_active_enemy_list[i].transform.position;
+
+            Vector3 vToTarget   =   target - pos;
+                    vToTarget.y =   0.0f;   //  高さを考慮しない
+			float	dist	    =   vToTarget.sqrMagnitude;
+
+			if( dist < near )
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+	public Transform GetNearEnemyTransform( Vector3 target, float maxDist )
+	{
+		int		nearID	= -1;
+		float	near	= maxDist*maxDist;
+
+		for( int i=0; i<m_active_enemy_list.Count; ++i )
+		{
+			Vector3 pos		    =	m_active_enemy_list[i].transform.position;
+
+            Vector3 vToTarget   =   target - pos;
+                    vToTarget.y =   0.0f;   //  高さを考慮しない
+			float	dist	    =   vToTarget.sqrMagnitude;
+
+			if( dist < near )
+			{
+				near	= dist;
+				nearID	= i;
+			}
+		}
+
+		if( nearID != -1 )
+		{
+			return m_active_enemy_list[ nearID ].transform;
+		}
+
+		return null;
+	}
+	public List<Transform> GetNearEnemyTransforms( Vector3 target, float maxDist )
+	{
+		List<Transform> list	= new List<Transform>();
+		float			near	= maxDist*maxDist;
+
+		for( int i=0; i<m_active_enemy_list.Count; ++i )
+		{
+			Vector3 pos		    =	m_active_enemy_list[i].transform.position;
+
+            Vector3 vToTarget   =   target - pos;
+                    vToTarget.y =   0.0f;   //  高さを考慮しない
+			float	dist	    =   vToTarget.sqrMagnitude;
+
+			if( dist < near )
+			{
+				list.Add( m_active_enemy_list[ i ].transform );
+			}
+		}
+
+		return list;
+	}
 }
--- RTS_And_TPS/Assets/Scripts/RTS/DefenseTower/TurretBullet.cs
+++ RTS_And_TPS/Assets/Scripts/RTS/DefenseTower/TurretBullet.cs
@@ -7,16 +7,16 @@ public class TurretBullet : MonoBehaviour
 	public float				m_speed			= 1.0f;
 	public float				m_chaseSpeed	= 1.0f;
 
-	private EnemyShell_Control  m_rEnemyShell   =   null;
+	private ReferenceWrapper	m_rEnemyShell   =   null;
     private LinkManager         m_rLinkManager  =   null;
 	private Transform		    m_target        =   null;
 
 	// Use this for initialization
 	void Start ()
 	{
-		m_rEnemyShell   =   GameObject.Find( "Enemy_Shell" ).GetComponent< EnemyShell_Control >();
+		m_rEnemyShell   =   GameObject.Find( "EnemySpawnRoot" ).GetComponent< ReferenceWrapper >();
         m_rLinkManager  =   FunctionManager.GetAccessComponent< LinkManager >( "LinkManager" );
-		m_target		= m_rEnemyShell.GetNearEnemyTransform( transform.position, float.MaxValue );
+		m_target		=	m_rEnemyShell.GetNearEnemyTransform( transform.position, float.MaxValue );
 	}
 	
 	// Update is called once per frame
