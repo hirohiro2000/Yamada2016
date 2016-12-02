3009acb48b4290c5fb43acec3312c7711a4dd716
shunsuketerauchi <kwgcr368@gmail.com>
2016-12-02 15:53:02 +0900

    途中

--- RTS_And_TPS/Assets/Scripts/AI/PQS/PointCreator.cs
+++ RTS_And_TPS/Assets/Scripts/AI/PQS/PointCreator.cs
@@ -15,10 +15,10 @@ public class PointCreator
         private float m_end_dist = .0f;
         private float m_add_dist = .0f; 
 
-        private float m_begin_radius = .0f;
-        private float m_end_radius = .0f;
-        private float m_add_radius = .0f;
-        private float m_current_radius;
+        private float m_begin_radian = .0f;
+        private float m_end_radian = .0f;
+        private float m_add_radian = .0f;
+        private float m_current_radian = .0f;
 
         private int m_max_create_point = 0;
         private int m_current_create_point = 0;
@@ -30,33 +30,43 @@ public class PointCreator
                                      float fov_exclusion,
                                      int  max_sampling_point)
         {
+            m_radius = radius;
+            m_current_create_point = 0;
             //生成除外した角度から有効角度角度を計算する
-            m_begin_radius = fov_exclusion / 180.0f * Mathf.PI;
-            m_end_radius = (Mathf.PI * 2.0f) - m_end_radius;
-            m_add_radius = (Mathf.PI * 2.0f) / rotate_detail;
+            m_begin_radian = fov_exclusion / 180.0f * Mathf.PI;
+            m_end_radian = (Mathf.PI * 2.0f) - m_begin_radian;
+            m_add_radian = (Mathf.PI * 2.0f) / rotate_detail;
+            m_current_radian = m_begin_radian;
 
             //初期の距離と加算していく距離を計算する
             m_end_dist = radius;
             m_begin_dist = distance_exclusion;
             m_add_dist = radius / dist_detail;
+            //begin_distが0の場合無駄ループが回るので
+            if (m_begin_dist <= 0)
+                m_current_dist = m_add_dist;
+            else
+                m_current_dist = m_begin_dist;
+
+            m_max_create_point = max_sampling_point;
         }
 
         public bool CreatePoint(ref Vector3 out_pos)
         {
 
-            out_pos.x = Mathf.Sin(m_current_radius) * m_current_dist;
+            out_pos.x = Mathf.Sin(m_current_radian) * m_current_dist;
             out_pos.y = .0f;
-            out_pos.z = Mathf.Cos(m_current_radius) * m_current_dist;  
+            out_pos.z = Mathf.Cos(m_current_radian) * m_current_dist;  
 
-            m_current_radius += m_add_radius;
+            m_current_radian += m_add_radian;
             m_current_create_point++;
             if (m_current_create_point > m_max_create_point)
                 return false;
 
-            if(m_current_radius >= m_end_radius)
+            if(m_current_radian >= m_end_radian)
             {
                 m_current_dist += m_add_dist;
-                m_current_radius = m_begin_radius;
+                m_current_radian = m_begin_radian;
                 //0.001はradiusが5の場合current_distが5に来たとき誤差で判定を間違えないため
                 if (m_current_dist >= m_radius + 0.001)
                     return false;
@@ -66,7 +76,9 @@ public class PointCreator
 
         public void EndCreate()
         {
-
+            m_end_radian = .0f;
+            m_current_dist = .0f;
+            m_current_radian = .0f;
         }
 
     }
@@ -123,11 +135,6 @@ public class PointCreator
         float          owner_height,
         List<PointQuerySystem.PointData> out_list)
     {
-        //float dist_size = query_info.GetPointFieldSize() / query_info.GetFieldDetailY();
-        //float base_radius = (Mathf.PI * 2.0f)  / query_info.GetFieldDetailX();
-        //Vector3 base_position = new Vector3();
-        //float dist = .0f;
-
         m_circle_sampler.BeginCreate(query_info.GetPointFieldSize(),
             query_info.GetFieldDetailY(),
             query_info.GetFieldDetailX(),
@@ -140,8 +147,12 @@ public class PointCreator
         while (true)
         {
 
-            if (m_circle_sampler.CreatePoint(ref candidate_pos))
+            //生成ポイントの最大数を超えたら || サイズ以上のポイントを生成しだしたら終了する
+            if (!m_circle_sampler.CreatePoint(ref candidate_pos))
                 break;
+
+            //形状の座標がローカル空間なのでtargetまで持ってくる
+            candidate_pos += target_transform.position;
 
             //対象となる位置に対してのフィルタリングを行う
             if (!IsCanCreate(query_info, target_transform, candidate_pos, target_height))
@@ -155,34 +166,6 @@ public class PointCreator
             out_list.Add(new PointQuerySystem.PointData(hit_data.position));
         }
         m_circle_sampler.EndCreate();
-        ////NavMeshHit hit_data = new NavMeshHit();
-        //for (int dist_i = 0; dist_i < query_info.GetFieldDetailY(); dist_i++)
-        //{
-        //    dist = dist_size * dist_i;
-        //    for(int  rotate_i = 0; rotate_i < query_info.GetFieldDetailX(); rotate_i++)
-        //    {
-        //        base_position.x = Mathf.Sin(base_radius * rotate_i) * dist;
-        //        base_position.y = .0f;
-        //        base_position.z = Mathf.Cos(base_radius * rotate_i) * dist;
-        //        Vector3 candidate_pos = base_position + target_transform.position;
-
-        //        //対象となる位置に対してのフィルタリングを行う
-        //        if (!IsCanCreate(query_info,target_transform, candidate_pos, target_height))
-        //            continue;
-
-        //        //Pointを生成する
-        //        if (!NavMesh.SamplePosition(candidate_pos, out hit_data, .2f, NavMesh.AllAreas))
-        //        {
-        //            continue;
-        //        }
-
-        //         out_list.Add(new PointQuerySystem.PointData(hit_data.position));
-               
-        //    }//rotate_i
-
-        //}//dist_i
-
-      //  m_use_filter_list.Clear();
     }
 
     private bool IsCanCreate(
@@ -191,10 +174,10 @@ public class PointCreator
         Vector3 candidate_pos,float target_height)
     {
         //まずは計算の軽いBaseFilterでフィルタリングを行う
-        if (!m_basic_filter.IsCanCreate(query_info,target, candidate_pos))
-        {
-            return false;
-        }
+        //if (!m_basic_filter.IsCanCreate(query_info,target, candidate_pos))
+        //{
+        //    return false;
+        //}
         //Queryのフィルタをパスすれば生成可能とする
         return query_info.FilteringCandidatePoint(target, candidate_pos, target_height);
     }
