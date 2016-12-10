a5a6dc6d13ce8f9b84e43c92e74c45663480a6a7
nakano <h2nakano@gmail.com>
2016-12-10 16:56:55 +0900

    近い距離で衝突した弾の軌跡がきれいに消えるように修正

--- RTS_And_TPS/Assets/Scripts/TPS/BeginTrail.cs
+++ RTS_And_TPS/Assets/Scripts/TPS/BeginTrail.cs
@@ -3,26 +3,44 @@ using System.Collections;
 
 //ParentFollowerトレイル用に拡張するスクリプト
 [RequireComponent(typeof(ParentFollower))]
+[RequireComponent(typeof(TrailRenderer))]
 public class BeginTrail : MonoBehaviour {
 
 	ParentFollower follower;
+	TrailRenderer trail;
+	public Material alphaMat;
+	Material startMat;
 	int c = 2;
+	float alpha;
 	// Use this for initialization
 	void Awake () {
 		follower = GetComponent<ParentFollower>();
-		follower.enabled = false;
+		trail = GetComponent<TrailRenderer>();
+		startMat = trail.material;
+        follower.enabled = false;
     }
 
 	// Update is called once per frame
 	void Update () {
 		c--;
-		if(c > 0)
+		if (follower.followParent != null)
 		{
-            transform.position = transform.position + (follower.followParent.position - transform.position).normalized * 0.001f;
+
+			if (c > 0)
+			{
+				transform.position = transform.position + (follower.followParent.position - transform.position).normalized * 0.001f;
+			}
+			else
+			{
+				follower.enabled = true;
+			}
 		}
-		else
+
+		if(follower.followParent == null)
 		{
-			follower.enabled = true;
-		}
+			alpha += 1.0f / trail.time * Time.deltaTime;
+			trail.material.Lerp(startMat, alphaMat, alpha);
+
+        }
 	}
 }
