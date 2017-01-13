41bb5818d1e104ec4f143fe5d9aaba611ff5ee0d
Sengoku39 <mindofevil39@gmail.com>
2017-01-13 14:56:09 +0900

    ステージ軽量化 ステージ最適化

--- RTS_And_TPS/Assets/Scripts/ForEdit/EditBox_Control.cs
+++ RTS_And_TPS/Assets/Scripts/ForEdit/EditBox_Control.cs
@@ -5,16 +5,68 @@ using   System.Collections.Generic;
 
 public class EditBox_Control : MonoBehaviour {
 
-    public  float       c_BaseScale     =   1.0f;
-    public  bool        c_CombineMesh   =   false;
-    public  bool        c_SetMaterial   =   true;
-    public  bool        c_SetUV         =   true;
+    public  float       c_BaseScale         =   1.0f;
+    public  bool        c_CombineBox        =   false;
+    public  bool        c_CombineMesh       =   false;
+    public  bool        c_CreateCollider    =   false;
+    public  bool        c_SetMaterial       =   true;
+    public  bool        c_SetUV             =   true;
 
-    public  Material[]  c_Material      =   null;
+    public  Material[]  c_Material          =   null;
+    public  Material[]  c_MaterialCache     =   null;
 
 	// Use this for initialization
 	void    Start()
     {
+        //  ポリゴンのクローンを作成
+        if( c_CombineBox ){
+            //  クローンの入れ物を作成
+            GameObject  rCloneShell =   new GameObject();
+            Transform   rCloneTrans =   rCloneShell.transform;
+            rCloneTrans.position    =   transform.position;
+            rCloneTrans.rotation    =   transform.rotation;
+            rCloneTrans.parent      =   transform;
+            rCloneShell.name        =   "CloneShell";
+
+            //  クローンを作成
+            Vector3     size        =   transform.localScale / c_BaseScale;
+            for( int i = 0; i < transform.childCount - 1; i++ ){
+                Vector2     uvScale =   GetUVScale( size, i );
+
+                Transform   rChild  =   transform.GetChild( i );
+                GameObject  rObj    =   CreatePolygon( rChild.position, rChild.eulerAngles, rChild.lossyScale, uvScale.x, uvScale.y );
+                Transform   rTrans  =   rObj.transform;
+                rTrans.parent       =   rCloneTrans;
+                rTrans.name         =   rChild.name + "[ Clone ]";
+            }
+
+            //  マテリアルを設定
+            int loopCount   =   Mathf.Min( c_Material.Length, transform.childCount );
+            for( int i = 0; i < loopCount; i++ ){
+                rCloneTrans.GetChild( i ).GetComponent< Renderer >().material =   c_Material[ i ];
+            }
+
+            //  クローン以外を破棄
+            {
+                rCloneTrans.parent  =   null;
+                int     numLoop     =   transform.childCount;
+                for( int i = 0; i < numLoop; i++ ){
+                    DestroyImmediate( transform.GetChild( 0 ).gameObject );
+                }
+
+                numLoop   =   rCloneTrans.childCount;
+                for( int i = 0; i < numLoop; i++ ){
+                    rCloneTrans.GetChild( 0 ).parent    =   transform;
+                }
+                DestroyImmediate( rCloneTrans.gameObject );
+            }
+
+            //  メッシュを作成 
+            CreateMesh();
+
+            return;
+        }
+
         //  マテリアル初期化
         if( c_SetMaterial ){
             SetMaterial();
@@ -35,13 +87,13 @@ public class EditBox_Control : MonoBehaviour {
         //this.enabled    =   false;
 	}
 	
-    //  メッシュ作成
-    void    CreateMesh()
+    //  メッシュ作成  
+    void        CreateMesh()
     {
         //  合体させるメッシュの数
         int numMesh =   transform.childCount;
 
-        //  壁と斜面をリストにまとめる
+        //  リストにまとめる
         List< Transform >   rTransList  =   new List< Transform >();
         for( int i = 0; i < transform.childCount; i++ ) rTransList.Add( transform.GetChild( i ) );
 
@@ -58,6 +110,10 @@ public class EditBox_Control : MonoBehaviour {
         //  プレハブを作成
         MeshFilter      rFilter     =   gameObject.AddComponent< MeshFilter >();
         MeshRenderer    rRenderer   =   gameObject.AddComponent< MeshRenderer >();
+        MeshCollider    rCollider   =   null;
+        if( c_CreateCollider ){
+            rCollider   =   gameObject.AddComponent< MeshCollider >();
+        }
 
         //  準備開始
         for( int i = 0; i < rTransList.Count; i++ ){
@@ -75,6 +131,40 @@ public class EditBox_Control : MonoBehaviour {
         transform.eulerAngles   =   Vector3.zero;
         transform.localScale    =   Vector3.one;
 
+        //  マテリアルの数を調べる
+        List< MeshRenderer >    rMatList    =   new List< MeshRenderer >();
+        for( int i = 0; i < combRender.Length; i++ ){
+            bool    isFounded   =   false;
+            //  すでに発見されているマテリアルかどうかをチェック
+            for( int m = 0; m < rMatList.Count; m++ ){
+                if( combRender[ i ].material.name != rMatList[ m ].material.name )  continue;
+        
+                //  すでに発見されていた
+                isFounded       =   true;
+                break;
+            }
+
+            //  未知の材質だった場合は登録
+            if( !isFounded ){
+                rMatList.Add( combRender[ i ] );
+            }
+        }
+
+        //  材質順にセットしなおす
+        List< Transform >       rMTransList =   new List< Transform >();
+        List< CombineInstance > rMCombList  =   new List< CombineInstance >();
+        List< MeshRenderer >    rMRendList  =   new List< MeshRenderer >();
+        for( int m = 0; m < rMatList.Count; m++ ){
+            for( int i = 0; i < numMesh; i++ ){
+                if( combRender[ i ].material.name != rMatList[ m ].material.name )  continue;
+
+                //  リストに登録
+                rMTransList.Add( rTransList[ i ] );
+                rMCombList.Add( combine[ i ] );
+                rMRendList.Add( combRender[ i ] );
+            }
+        }
+
         //  メッシュをひとつにまとめる
         {
             //  メッシュ作成
@@ -89,8 +179,8 @@ public class EditBox_Control : MonoBehaviour {
             {
                 int             indexOffset =   0;
                 for( int m = 0; m < numMesh; m++ ){
-                    Transform   rTrans  =   rTransList[ m ];
-                    Mesh        rMesh   =   combine[ m ].mesh;
+                    Transform   rTrans  =   rMTransList[ m ];//rTransList[ m ];
+                    Mesh        rMesh   =   rMCombList[ m ].mesh;//combine[ m ].mesh;
 
                     //  頂点
                     for( int i = 0; i < rMesh.vertices.Length; i++ ){
@@ -120,35 +210,59 @@ public class EditBox_Control : MonoBehaviour {
             rFilter.mesh.RecalculateBounds();
 
             //  サブメッシュの数
-            rFilter.mesh.subMeshCount   =   numMesh;
+            rFilter.mesh.subMeshCount   =   rMatList.Count;//numMesh;
+
+            //  コライダー設定
+            if( rCollider ){
+                rCollider.sharedMesh    =   rFilter.mesh;
+            }
 
             //  サブメッシュの構成情報を設定 
             {
                 int indexOffset     =   0;
-                for( int m = 0; m < numMesh; m++ ){
-                    //  サブメッシュのインデックスリストを作成
-                    Mesh        rMesh   =   combine[ m ].mesh;
+                for( int mt = 0; mt < rMatList.Count; mt++ ){
+                    //  サブメッシュ内の三角形リスト
                     List< int > triList =   new List< int >();
-                    for( int i = 0; i < rMesh.triangles.Length; i++ ){
-                        triList.Add( rMesh.triangles[ i ] + indexOffset );
+
+                    //  材質が一致する三角形をサブメッシュにまとめる
+                    for( int m = 0; m < numMesh; m++ ){
+                        //  材質チェック
+                        if( rMRendList[ m ].material.name != rMatList[ mt ].material.name ) continue;
+
+                        //  サブメッシュのインデックスリストを作成
+                        Mesh        rMesh   =   rMCombList[ m ].mesh;
+                        for( int i = 0; i < rMesh.triangles.Length; i++ ){
+                            triList.Add( rMesh.triangles[ i ] + indexOffset );
+                        }
+                        //  インデックスをずらす
+                        indexOffset     +=  rMesh.vertexCount;
                     }
 
                     //  サブメッシュ登録
-                    rFilter.mesh.SetTriangles( triList.ToArray(), m );
-
-                    //  インデックスをずらす
-                    indexOffset     +=  rMesh.vertexCount;
+                    rFilter.mesh.SetTriangles( triList.ToArray(), mt );
                 }
             }
 
-            //  共有メッシュに設定
-            rFilter.sharedMesh      =   rFilter.mesh;
+            //  マテリアルを生成 
+            rRenderer.materials     =   new Material[ rMatList.Count ];
 
-            //  マテリアルを設定
-            rRenderer.materials     =   new Material[ numMesh ];
-            for( int m = 0; m < numMesh; m++ ){
-                rRenderer.materials[ m ].CopyPropertiesFromMaterial( combRender[ m ].material );
+            //  マテリアルを設定 
+            Material[]  rMaterials  =   rRenderer.materials;
+            for( int m = 0; m < rMatList.Count; m++ ){
+                Material    rMaterial   =   rMatList[ m ].material;//combRender[ m ].sharedMaterial;
+                if( c_MaterialCache != null ){
+                    for( int c = 0; c < c_MaterialCache.Length; c++ ){
+                        if( c_MaterialCache[ c ] == null )                              continue;
+                        if( rMaterial.name.IndexOf( c_MaterialCache[ c ].name ) < 0 )   continue;
+
+                        rMaterial   =   c_MaterialCache[ c ];
+                        break;
+                    }
+                }
+                
+                rMaterials[ m ]     =   rMaterial;
             }
+            rRenderer.materials     =   rMaterials;
         }
 
         //  配置情報復元
@@ -166,6 +280,44 @@ public class EditBox_Control : MonoBehaviour {
 
         //  静的オブジェクトに戻す
         gameObject.isStatic =   true;
+    }
+    GameObject  CreatePolygon( Vector3 _Position, Vector3 _Angles, Vector3 _Scale, float _UVScaleX, float _UVScaleY )
+    {
+        //  メッシュ作成
+        GameObject  rObj    =   new GameObject();
+        Transform   rTrans  =   rObj.transform;
+        Mesh        rMesh   =   new Mesh();
+        rMesh.vertices      =   new Vector3[]{
+            new Vector3(  0.5f,  0.5f ),
+            new Vector3( -0.5f, -0.5f ),
+            new Vector3( -0.5f,  0.5f ),
+            new Vector3(  0.5f, -0.5f ),
+        };
+        rMesh.triangles     =   new int[]{
+            0,  1,  2,
+            0,  3,  1
+        };
+        rMesh.uv            =   new Vector2[]{
+            new Vector2( 0.0f,              1.0f * _UVScaleY ),
+            new Vector2( 1.0f * _UVScaleX,  0.0f ),
+            new Vector2( 1.0f * _UVScaleX,  1.0f * _UVScaleY ),
+            new Vector2( 0.0f,              0.0f ),
+        };
+
+        //  法線情報計算
+        rMesh.RecalculateNormals();
+        rMesh.RecalculateBounds();
+
+        //  描画設定
+        rObj.AddComponent< MeshFilter >().sharedMesh    =   rMesh;
+        rObj.AddComponent< MeshRenderer >().material    =   c_Material[ 0 ];
+
+        //  配置設定
+        rTrans.position     =   _Position;
+        rTrans.localScale   =   _Scale;
+        rTrans.eulerAngles  =   _Angles;
+
+        return  rObj;
     }
     void    SetUV( int _X, int _Y, int _Z )
     {
@@ -189,6 +341,69 @@ public class EditBox_Control : MonoBehaviour {
 
             Renderer    rRenderer   =   transform.GetChild( i ).GetComponent< Renderer >();
             rRenderer.material.SetTextureScale( "_MainTex", uvScale );
+        }
+    }
+    Vector2 GetUVScale( Vector3 _Size, int _FaceID )
+    {
+        _Size.x  =   Mathf.Max( 1, _Size.x );
+        _Size.y  =   Mathf.Max( 1, _Size.y );
+        _Size.z  =   Mathf.Max( 1, _Size.z );
+
+        int[]   xAffectFace =   {   0,  1,  3,  5   };
+        int[]   yAffectFace =   {   1,  2,  3,  4   };
+        int[]   zAffectFace =   {   0,  2,  4,  5   };
+
+        Vector2 uvScale =   Vector2.one;
+        
+        if( CheckInNumber( _FaceID, xAffectFace ) ) uvScale.x   +=  Mathf.Max( _Size.x - 1, 0 );
+        if( CheckInNumber( _FaceID, yAffectFace ) ) uvScale.y   +=  Mathf.Max( _Size.y - 1, 0 );
+        if( CheckInNumber( _FaceID, zAffectFace ) ){
+            if( _FaceID == 0 || _FaceID == 5 )      uvScale.y   +=  Mathf.Max( _Size.z - 1, 0 );
+            else                                    uvScale.x   +=  Mathf.Max( _Size.z - 1, 0 );
+        }
+
+        return  uvScale;
+    }
+    void    SetUVToMesh( int _X, int _Y, int _Z )
+    {
+        //  合体させるメッシュの数
+        int numMesh =   transform.childCount;
+
+        //  壁と斜面をリストにまとめる
+        List< Transform >   rTransList  =   new List< Transform >();
+        for( int i = 0; i < transform.childCount; i++ ) rTransList.Add( transform.GetChild( i ) );
+
+        //  合体させるためのデータを作成 
+        //CombineInstance[]   combine     =   new CombineInstance[ rTransList.Count ];
+        //MeshRenderer[]      combRender  =   new MeshRenderer[ rTransList.Count ];
+        //for( int i = 0; i < combine.Length; i++ ){
+        //    combine[ i ].mesh       =   rTransList[ i ].GetComponent< MeshFilter >().sharedMesh;
+        //    combine[ i ].transform  =   rTransList[ i ].localToWorldMatrix;
+
+        //    combRender[ i ]         =   rTransList[ i ].GetComponent< MeshRenderer >();
+        //}
+
+        //  ＵＶ設定
+        {
+            Mesh            rMesh   =   rTransList[ 0 ].GetComponent< MeshFilter >().sharedMesh;
+            List< Vector2 > uvList  =   new List< Vector2 >();
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            uvList.Add( new Vector2( 1, 0 ) );
+            uvList.Add( new Vector2( 0, 1 ) ); 
+            uvList.Add( new Vector2( 0, 0 ) );
+            uvList.Add( new Vector2( 1, 1 ) );
+
+            //  設定 
+            rMesh.SetUVs( 0, uvList );
         }
     }
     void    SetMaterial()
