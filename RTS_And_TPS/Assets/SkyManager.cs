986cf7172e846adf159dc9d26968e16f42d53add
nakano <h2nakano@gmail.com>
2017-01-19 14:56:38 +0900

    skyboxが滑らかに変化します

--- RTS_And_TPS/Assets/SkyManager.cs
+++ RTS_And_TPS/Assets/SkyManager.cs
@@ -16,17 +16,26 @@ public class SkyManager : MonoBehaviour {
     private SkyData         m_rCurData      =   null;
     private Cloud_Control   m_rCloudControl =   null;
 
+	[SerializeField, Range(.1f,10.0f)]
+	public float m_ChangeTime = 1.0f;
+
+	private float m_BlendValue;
+
 	// Use this for initialization
 	void    Start()
     {
         m_rCloudControl =   transform.FindChild( "CloudSystem" ).GetComponent< Cloud_Control >();
-	}
+		ChangeMaterialTexture(c_SkyData[0].skyMaterial,1.0f);
+    }
 	
 	// Update is called once per frame
 	void    Update()
     {
-
-	}
+		m_BlendValue += Time.deltaTime / m_ChangeTime;
+		if (m_BlendValue > 1.0f)
+			m_BlendValue = 1.0f;
+        RenderSettings.skybox.SetFloat("_SkyBlend", m_BlendValue);
+    }
 
     public  void    ChangeSky( int _SkyIndex )
     {
@@ -40,11 +49,55 @@ public class SkyManager : MonoBehaviour {
         //  データが同じ場合は変更処理を行わない
         if( rData == m_rCurData )   return;
 
-        RenderSettings.skybox   =   rData.skyMaterial;
+		//RenderSettings.skybox   =   rData.skyMaterial;
+		ChangeMaterialTexture(rData.skyMaterial,.0f);
+		m_BlendValue = .0f;
 
-        m_rCloudControl.SetSpeed( rData.cloudSpeed );
+		m_rCloudControl.SetSpeed( rData.cloudSpeed );
         m_rCloudControl.SetMaterial( rData.cloudMaterial );
 
         m_rCurData  =   rData;
     }
+
+	void ChangeMaterialTexture(Material New,float value)
+	{
+		string[] PrevPropertys =
+		{
+			"_FrontTex_01",
+			"_BackTex_01",
+			"_LeftTex_01",
+			"_RightTex_01",
+			"_UpTex_01",
+			"_DownTex_01",
+		};
+		string[] NextPropertys =
+		{
+			"_FrontTex_02",
+			"_BackTex_02",
+			"_LeftTex_02",
+			"_RightTex_02",
+			"_UpTex_02",
+			"_DownTex_02",
+		};
+		string[] NewPropertys =
+		{
+			"_FrontTex",
+			"_BackTex",
+			"_LeftTex",
+			"_RightTex",
+			"_UpTex",
+			"_DownTex",
+		};
+		for (int i = 0; i < 6; i++)
+		{
+			//前のNextをPrevに
+			RenderSettings.skybox.SetTexture(PrevPropertys[i], RenderSettings.skybox.GetTexture(NextPropertys[i]));
+			//引数のマテリアルのテクスチャをNextに
+			RenderSettings.skybox.SetTexture(NextPropertys[i], New.GetTexture(NewPropertys[i]));
+			m_BlendValue = value;
+			RenderSettings.skybox.SetFloat("_SkyBlend", m_BlendValue);
+
+		}
+
+	}
 }
