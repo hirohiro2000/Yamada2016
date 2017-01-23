c186c6bc5af027eb9413d9708f98c7960e41d8dc
nakano <h2nakano@gmail.com>
2017-01-24 02:04:42 +0900

    足踏みと射撃エフェクト追加

---
+++ RTS_And_TPS/Assets/Scripts/Effect/HitEffect_Control.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitEffect_Control : MonoBehaviour {
+	public GameObject[] c_Emission = null;
+
+	void OnCollisionEnter(Collision _rCollision)
+	{
+		Emit();
+	}
+
+	void OnTriggerEnter(Collider _rCollision)
+	{
+		Emit();
+	}
+
+	void Emit()
+	{
+		if (c_Emission == null) return;
+		if (c_Emission.Length == 0) return;
+
+		for (int i = 0; i < c_Emission.Length; i++)
+		{
+			GameObject rEmission = c_Emission[i];
+			if (!rEmission) continue;
+
+			Instantiate(rEmission, transform.position, transform.rotation);
+
+		}
+	}
+}
