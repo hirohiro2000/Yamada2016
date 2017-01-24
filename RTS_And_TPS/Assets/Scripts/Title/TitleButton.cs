faf221c99410cb783033a5d88d8d3f6be5b5a9f7
shunsuke <kwgcr368@ybb.ne.jp>
2017-01-24 11:19:44 +0900

    タイトル画面改良 木を伐採して軽くした

---
+++ RTS_And_TPS/Assets/Scripts/Title/TitleButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleButton : MonoBehaviour {
+
+    GameObject safe_image = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        safe_image = transform.GetChild(0).gameObject;
+        safe_image.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Execute()
+    {
+        safe_image.SetActive(!safe_image.activeInHierarchy);
+    }
+}
