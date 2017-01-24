618ed9a92c9a291f5b6bcb4dffed582def523185
marumaru-pg <marumaru.pg@gmail.com>
2017-01-24 17:45:40 +0900

    未来

---
+++ RTS_And_TPS/Assets/Scripts/RTS/DeviationCalculator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeviationCalculator : MonoBehaviour
+{
+	private Transform	m_eye = null;
+	private Vector3		m_cur;
+	private Vector3		m_prev;
+	private Vector3		m_vel;
+
+	// Use this for initialization
+	void Start ()
+	{
+		m_prev	= m_cur = transform.position;
+		m_eye	= transform.FindChild("Eye");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		m_prev	= m_cur;
+		m_cur	= ( m_eye == null )? transform.position : m_eye.position;
+		m_vel	= m_cur - m_prev;
+	}
+
+	public Vector3 Get()
+	{
+		return transform.position + m_vel;
+	}
+}
