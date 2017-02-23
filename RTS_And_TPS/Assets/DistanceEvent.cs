ca10a600adb4a0f1ad2187e0a911764c6fec2008
nakano <h2nakano@gmail.com>
2017-02-23 14:15:31 +0900

    UFOの変更を適用

--- RTS_And_TPS/Assets/DistanceEvent.cs
+++ RTS_And_TPS/Assets/DistanceEvent.cs
@@ -13,6 +13,14 @@ public class DistanceEvent : MonoBehaviour {
 
 	public bool m_OneFrame = false;
 
+	public bool m_IgnoreX = false;
+
+	public bool m_IgnoreY = false;
+
+	public bool m_IgnoreZ = false;
+
+	public bool m_Once = false;
+
 	LinkManager m_rLinkManager = null;
 
 	bool m_beforeFlag = false;
@@ -21,14 +29,25 @@ public class DistanceEvent : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		m_rLinkManager  =   FunctionManager.GetAccessComponent< LinkManager >( "LinkManager" );
-		m_Events.Invoke();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		m_Flag = false;
 
-		float distance =( transform.position - m_rLinkManager.m_rLocalPlayer.transform.position ).sqrMagnitude;
+		Vector3 vec = ( transform.position - m_rLinkManager.m_rLocalPlayer.transform.position );
+
+		if(m_IgnoreX == true)
+			vec.x = .0f;
+
+		if(m_IgnoreY == true)
+			vec.y = .0f;
+
+		if(m_IgnoreZ == true)
+			vec.z = .0f;
+
+
+		float distance = vec.sqrMagnitude;
 
 		distance = distance - (m_Distance * m_Distance);
 
@@ -38,14 +57,23 @@ public class DistanceEvent : MonoBehaviour {
 		if(distance < .0f)
 		{
 			if(m_OneFrame == false)
+			{
 				m_Events.Invoke();
+				if(m_Once == true)
+					enabled = false;
+			}
+
 			else
 				m_Flag = true;
 		}
 		if(m_OneFrame == true)
 		{
 			if(m_Flag == true && m_beforeFlag == false)
+			{
 				m_Events.Invoke();
+				if(m_Once == true)
+					enabled = false;
+			}
 		}
 		m_beforeFlag = m_Flag;
 	}
